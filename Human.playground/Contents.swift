62926129f7189fb0d09ee481db0c5821bc1f7968
NeoTerminal <samyy.abcd@gmail.com>
2015-10-06 19:51:59 -0400

    edited/completed human code

---
+++ Human.playground/Contents.swift
@@ -0,0 +1,346 @@
+class HumanZombie{
+    
+    //properties
+    
+    let rottingFlesh: String
+    let hobblingGait: String
+    let horridGrowl: String
+    init(rottingFlesh: String, hobblingGait: String, horridGrowl: String) {
+        
+        self.rottingFlesh
+        self.hobblingGait
+        self.horridGrowl
+    
+
+    
+ }
+}
+
+func rotBreakAndSmells(rot:String, Break: String, Smell: String)
+{
+    
+}
+
+
+class BloodShotEyes: HumanZombie{
+    
+
+    
+    //properties
+    
+    var bigPupils: String
+    var redCorneas: String
+    var lowGaze: String
+init(bigPupils: String, redCorneas: String, lowGaze: String) {
+    
+    
+        self.bigPupils
+        self.redCorneas
+        self.lowGaze
+}
+}
+func stareOozeAndRoll(stare:String, Ooze: String, Roll: String)
+{
+    
+}
+
+
+class Ears: HumanZombie{
+
+        //properties
+        
+    let pointy: String
+    let hairy: String
+    let rotting: String
+    init(pointy: String, hairy: String, rotting: String) {
+        
+    }
+}
+func hearOozeAndTwitch(hear:String, Ooze: String, Twitch: String)
+{
+    
+}
+
+class Nose: HumanZombie{
+    //properties
+    
+    let purple: String
+    let hairy: String
+    let infected: String
+    init(purple:String, hairy: String
+        , infected: String) {
+    }
+}
+func smellFleshFindFoodAndSniffKind(smellFlesh: String, findFood: String,  sniffKind)
+{
+    
+}
+
+class mouth: HumanZombie{
+    //properties
+    
+    let badTeeth:String
+    let grayTongue:String
+    let badBreath:String
+    init(badTeeth: String, grayTongue: String, badBreath: String) {
+        
+    }
+    
+}
+func chewFleshDrinkBloodAndEatWorms(chewFlesh :String, drinkBlood: String, eatWorms) {
+    
+}
+
+class Drool: HumanZombie{
+    //properties
+    
+    let smelly: String
+    let dirty: String
+    let acidic: String
+    init(smelly: String, dirty: String, acidic: String) {
+        
+    }
+}
+func coverFleshDigestFleshAndBreakdownFlesh(coverFlesh: String, digestFlesh: String, breakdownFlesh) {
+    
+}
+
+class Chest: HumanZombie{
+    //properties
+    
+    let rippedOpen: String
+    let decayed: String
+    let boney: String
+    init(rippedOpen: String, decayed: String, boney: String) {
+        
+    }
+}
+func holdsBugsHoldsLarvaAndHoldsSnakes(holdsBugs:String, holdsLarva: String, holdsSnakes: String)
+{
+    
+}
+
+class Stomach: HumanZombie{
+    // properties
+    
+    let infection: String
+    let disease: String
+    let acidic: String
+    init(infection: String, disease: String, acidic: String) {
+        
+    }
+}
+func breakdownFleshHoldsBloodAndHoldsWorms(breakdownFlesh: String, holdsBlood: String, holdsWorms)
+{
+    
+}
+
+class Back: HumanZombie{
+    //properties
+    
+    let slump: String
+    let rotting: String
+    let solid: String
+    init(slump: String, rotting: String, solid:String) {
+        
+    }
+}
+func keepBodyUpProvideBalanceProvideProtection(keepBodyUp: String, provideBalance:String, provideProtection)
+{
+    
+}
+
+class Arms: HumanZombie{
+    //properties
+    
+    let long: String
+    let decayed: String
+    let flimsy: String
+    init(long: String, decayed: String, flimsy: String) {
+        
+    }
+}
+func liftVictimsBlockBulletsAndDeflectFire(liftVictims: String, blockBullets: String, deflectFire: String)
+{
+    
+}
+
+class Hands: HumanZombie{
+    //properties
+    
+    let wrinkly: String
+    let veiny: String
+    let skinless: String
+    init(wrinkly: String, veiny: String, skinless: String)
+    {
+        
+    }
+}
+func grabVictimsRipFleshAndOpenDoors(grabVictims: String, ripFlesh: String, openDoors: String)
+{
+    
+}
+
+class Hips: HumanZombie{
+    //properties
+    
+    let small:String
+    let boney:String
+    let sharp:String
+    init(small:String,boney:String, sharp:String)
+    {
+        
+    }
+}
+func holdsTorsoHoldsAcidAndHoldsPantsUp(holdsTorso:String, holdsAcid:String, holdsPantsUp)
+{
+    
+}
+
+class Quads: HumanZombie{
+    //properties
+    
+    let skinny: String
+    let shaky: String
+    let weak: String
+    init(skinny:String, shaky:String, weak:String)
+    {
+        
+    }
+}
+func jumpWallsChaseVictimsAndSquatToEat(jumpWalls:String, chaseVictims:String, squatToEat:String)
+{
+    
+}
+
+class Knees: HumanZombie{
+    //properties
+    
+    let tiny:String
+    let sharp:String
+    let wobbly:String
+    init(tiny:String, sharp:String, wobbly:String)
+    {
+        
+    }
+}
+func kickVictimsDeflectBulletsAndStabFood(kickVictims:String, deflectBullets:String, stabFood:String)
+{
+    
+}
+
+class Calves: HumanZombie{
+    //properties
+    
+    let small:String
+    let skinless:String
+    let purple:String
+    init(small:String, sklinless:String, purple:String)
+    {
+        
+    }
+}
+func runFastHoldsQuadsBalancesLegs(runFast:String, holdsQuads:String, balancesLegs:String)
+{
+    
+}
+
+class Ankles: HumanZombie{
+    //properties
+    
+    let shaky:String
+    let tiny:String
+    let cracked:String
+    init(shaky:String, tiny:String, cracked:String)
+    {
+        
+    }
+}
+func holdsCalvesRandomlyBreaksAndPivotsFeet(holdsCalves:String, randomlyBreaks:String, pivotsFeet:String)
+{
+    
+}
+func chaseVictimsJumpHighAndPopBottleCaps(chaseVictims:String, jumpHigh:String, popBottleCaps:String)
+{
+    
+}
+
+class Feet: HumanZombie{
+    //properties
+    
+    let dirty:String
+    let scales:String
+    let decayed:String
+    init(dirty:String, scales:String, decayed:String)
+    {
+        
+    }
+}
+func crushVictimsWalkAndRun(crushVictims:String, walk:String, run:String)
+{
+    
+}
+
+class Toes: HumanZombie{
+    //properties
+    
+    let sharp:String
+    let dirty:String
+    let black:String
+    init(sharp:String, dirty:String, black:String)
+    {
+        
+    }
+}
+func grabSpoonsClimbPipesAndStabVictims(grabSpoons:String, climbPipes:String, stabVictims)
+{
+    
+}
+
+class Lungs: HumanZombie{
+    //properties
+    
+    let black:String
+    let weak:String
+    let decayed:String
+    init(blacl:String, weak:String, decayed:String)
+    {
+        
+    }
+}
+func expandInhaleSmellsAndHoldsWorms(expand:String, inhaleSmells:String, holdsWorms:String)
+{
+    
+}
+
+class Nails: HumanZombie{
+    //properties
+    
+    let sharp:String
+    let long:String
+    let dirty:String
+    init(sharp:String, long:String, dirty:String)
+    {
+        
+    }
+}
+func stabVictimsCutFleshAndTearClothes(stabVictims:String, cutFlesh:String, tearClothes:String)
+{
+    
+}
+
+class Elbows: HumanZombie {
+    //properties
+    
+    let pointy:String
+    let metallic:String
+    let dry:String
+    init(pointy:String, metallic:String, dry:String)
+    {
+        
+    }
+}
+func deflectBulletsGuardFaceAndStabVictims(deflectBullets:String, guardFace:String, stabVictims:String)
+{
+    
+}
