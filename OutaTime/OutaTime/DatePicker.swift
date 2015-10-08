5d48fd4c7a13c2b9be0cea2059fb4ab7dab42354
NeoTerminal <samyy.abcd@gmail.com>
2015-10-08 17:22:04 -0400

    started project added iucontroller

---
+++ OutaTime/OutaTime/DatePicker.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  OutaTime
+//
+//  Created by Samuel Shaw on 10/8/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
---
+++ OutaTime/OutaTime/OutaTimeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Samuel Shaw on 10/8/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class OutaTimeViewController: UIViewController
+{
+    
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartLabel: UILabel!
+    @IBOutlet weak var milesPerHourLabel: UILabel!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
