6fd190e1c885a7ec16bcc3bfdc9b7fe49eea768b
NeoTerminal <samyy.abcd@gmail.com>
2015-11-11 12:25:08 -0500

    initial commit

---
+++ 28 -- High Voltage Redux/HIGH VOLTAGE REDUX/HIGH VOLTAGE REDUX/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HIGH VOLTAGE REDUX
+//
+//  Created by Samuel Shaw on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
